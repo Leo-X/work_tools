6a13884666bfa2b4d38f4d8552151d55e3904d17
Leo-X <wonderanything1@gmail.com>
2019-03-15 18:06:16 +0800

    dd

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 let fib = n => {
     let f1 = 1;
     let f2 = 1;
-    for (let i = 0; i < n - 1; i++) {
+    for (let i = 0; i < n-2; i++) {
         [f1, f2] = [f2, f2 + f1];
     }
     return f2
 }
-console.log(`fib: ${ fib(50) }`);
+console.log(`fib: ${ fib(4) }`);
