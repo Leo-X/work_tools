863652715135f8b1cb5743b3bc53ed22cb134b51
Leo-X <wonderanything1@gmail.com>
2019-05-15 15:08:07 +0800

    dd

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//     trailingComma: 'none',
+//     tabWidth: 4,
+//     semi: false,
+//     singleQuote: true,
+//     overrides: [
+//         {
+//             files: '*.test.js',
+//             options: {
+//                 semi: false,
+//                 parser: 'flow',
+//             }
+//         },
+//         {
+//             files: '.prettierrc',
+//             trailingComma: 'none',
+//             options: { parser: 'json' }
+//         }
+//     ]
+// }
